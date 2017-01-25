5dddf861e7535009a61542afaf3aca4cc3a84709
nlkassad <nlkassad@hotmail.com>
2017-01-25 12:55:44 -0500

    Bind click event to StorePicker
    -bind submit to StorePicker
    -prevents default reload
    -allows input to be passed to function

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -2,16 +2,18 @@ import React from 'react';
 import { getFunName } from "../helpers";
 
 class StorePicker extends React.Component {
-  goToStore () {
-    console.log('You clicked submit');
+  goToStore (event) {
+    event.preventDefault();
+    console.log(this.storeInput);
   }
 
   render() {
     return (
-      <form className="store-selector" onSubmit={ this.goToStore() }>
+      <form className="store-selector" onSubmit={ (e) => this.goToStore(e) }>
         { /* this is a comment */ }
         <h2>Please Enter a Store</h2>
-        <input type="text" required placeholder="Store Name" defaultValue={getFunName()}/>
+        <input type="text" required placeholder="Store Name" defaultValue={
+          getFunName()} ref={(input) => { this.storeInput = input }}/>
         <button type="submit">Visit Store </button>
       </form>
     )
