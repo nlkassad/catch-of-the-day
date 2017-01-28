b403cbdd25c8979f243858c4ebd25aec150efb0c
nlkassad <nlkassad@hotmail.com>
2017-01-28 13:47:53 -0500

    Add render total order
    -add totals for order
    -return order totals to order div

--- src/components/App.js
+++ src/components/App.js
@@ -11,6 +11,8 @@ class App extends React.Component {
 
     this.addFish = this.addFish.bind(this);
     this.loadSamples = this.loadSamples.bind(this);
+    this.addToOrder = this.addToOrder.bind(this);
+
     this.state = {
       fishes: {},
       order: {}
@@ -29,6 +31,11 @@ class App extends React.Component {
       fishes: sampleFishes
     });
   }
+  addToOrder(key) {
+    const order = {...this.state.order};
+    order[key] = order[key] + 1 || 1;
+    this.setState({ order: order });
+  }
   render() {
     return (
       <div className="catch-of-the-day">
@@ -38,12 +45,13 @@ class App extends React.Component {
             {
               Object
                 .keys(this.state.fishes)
-                .map(key => <Fish key={key} details={this.state.fishes[key]}/>)
+                .map(key => <Fish key={key} index={key} details={
+                  this.state.fishes[key]} addToOrder={this.addToOrder}/>)
 
             }
           </ul>
         </div>
-        <Order />
+        <Order fishes={this.state.fishes} order={this.state.order} />
         <Inventory addFish={this.addFish} loadSamples={this.loadSamples} />
       </div>
     )
