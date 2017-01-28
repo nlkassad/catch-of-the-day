da7d5ee1990ec14a5b512b3460928e2dc101789f
nlkassad <nlkassad@hotmail.com>
2017-01-28 12:22:00 -0500

    Add samples to state
    -add function to load samples to state
    -add button to call load sample function
    -add ability to load sample information to statue

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import Header from './Header';
 import Order from './Order';
 import Inventory from './Inventory';
+import sampleFishes from '../sample-fishes';
 
 class App extends React.Component {
   constructor() {
     super();
 
     this.addFish = this.addFish.bind(this);
+    this.loadSamples = this.loadSamples.bind(this);
     this.state = {
       fishes: {},
       order: {}
@@ -21,6 +23,11 @@ class App extends React.Component {
     // set state
     this.setState({ fishes: fishes });
   }
+  loadSamples() {
+    this.setState({
+      fishes: sampleFishes
+    });
+  }
   render() {
     return (
       <div className="catch-of-the-day">
@@ -28,7 +35,7 @@ class App extends React.Component {
           <Header tagline="Fresh Seafood Market"/>
         </div>
         <Order />
-        <Inventory addFish={this.addFish} />
+        <Inventory addFish={this.addFish} loadSamples={this.loadSamples} />
       </div>
     )
   }
