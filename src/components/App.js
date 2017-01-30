62c2177077b70568af13e0d95430957e9d5bd7d4
nlkassad <nlkassad@hotmail.com>
2017-01-30 03:05:51 -0500

    Add remove fish button and action
    -add button that removes fish inventory

--- src/components/App.js
+++ src/components/App.js
@@ -14,6 +14,8 @@ class App extends React.Component {
     this.loadSamples = this.loadSamples.bind(this);
     this.addToOrder = this.addToOrder.bind(this);
     this.updateFish = this.updateFish.bind(this);
+    this.removeFish = this.removeFish.bind(this);
+
 
 
     this.state = {
@@ -64,6 +66,13 @@ class App extends React.Component {
     this.setState({ fishes });
   }
 
+  removeFish(key) {
+    const fishes = {...this.state.fishes};
+    fishes[key] = null;
+    this.setState({ fishes });
+
+  }
+
   loadSamples() {
     this.setState({
       fishes: sampleFishes
@@ -102,6 +111,7 @@ class App extends React.Component {
           loadSamples={this.loadSamples}
           fishes={this.state.fishes}
           updateFish={this.updateFish}
+          removeFish={this.removeFish}
           />
       </div>
     )
